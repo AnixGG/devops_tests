7fa79b1752c006abeadf68dca9376cabdd69e82d
Nithon <g4zizullinn@yandex.ru>
2024-06-03 22:44:33 +0300

    check pytest 0

--- my_code/my_functions.py
+++ my_code/my_functions.py
@@ -1,26 +1,32 @@
 def plus_plus(a: int):
+    """двойной плюс"""
     return a + 2
 
 
 def minus_minus(a: int):
+    """двойной минус"""
     return a - 2
 
 
 def rock_and_roll(a: int, b: int):
+    """просто функция, полезная(не всегда)"""
     if a == b:
         return "and"
     return "no no no"
 
 
 def is_my_tshirt_size(size: int):
+    """проверяет размер футболки(шутка)"""
     if size == -1:
         return True
     return False
 
 
 def cm_to_inch(cm: int):
+    """см -> дюйм"""
     return cm * 0.393701
 
 
 def multic(a: int, b: int):
+    """умножение"""
     return a * b
